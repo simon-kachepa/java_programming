584999f156d1f1e13f66c335522f1bfd6a7f5eef
simon Kachepa <kachepasimon@gmail.com>
2026-06-02 01:09:36 +0200

    Validated the number of dice that a user enters to make sure no 0 or less entered

--- dice_rolling_game/RollingMultipleDice.java
+++ dice_rolling_game/RollingMultipleDice.java
@@ -16,15 +16,20 @@ public class RollingMultipleDice {
         System.out.print("Enter number of dice you want to roll: ");
         numberOfDice = scanner.nextInt();
         System.out.println("\n*********************************************\n");
+        if (numberOfDice > 0){
 
-        for (int i = 0; i < numberOfDice; i++){
-            rollResult = random.nextInt(1, 7);
-            dice(rollResult);
-            System.out.println("You rolled a " + rollResult + ".\n");
-            sum += rollResult;
+            for (int i = 0; i < numberOfDice; i++){
+                rollResult = random.nextInt(1, 7);
+                dice(rollResult);
+                System.out.println("You rolled a " + rollResult + ".\n");
+                sum += rollResult;
+            }
+            System.out.println("*********************************************\n");
+            System.out.println("Your total roll: " + sum + "\n");
         }
-        System.out.println("*********************************************\n");
-        System.out.println("Your total roll: " + sum + "\n");
+        else{
+            System.out.println("Number of dice must be more than 0.\n");
+        }
         System.out.println("***********************************************************************************\n");
 
 
