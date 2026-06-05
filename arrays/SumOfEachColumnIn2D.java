6e1c8831ad3359da0c17db6dea631afe11aab45f
simon Kachepa <kachepasimon@gmail.com>
2026-06-05 09:10:45 +0200

    Created a file SumOfEachColumnIn2D.java in arrays

--- arrays/SumOfEachColumnIn2D.java
+++ arrays/SumOfEachColumnIn2D.java
@@ -1,18 +1,30 @@
 public class SumOfEachColumnIn2D {
     public static void main(String[] args){
-        int[][] myArray = {{2, 3, 4},
+        int[][] myArray = {{2, 3, 4, 6},
                            {1, 4, 6},
-                           {5, 3, 1},
+                           {5, 3, 1, 9, 0, 1},
                            {3, 2, 1}
                           };
+        int maxLength = myArray[0].length;
 
-        int sum = 0;
-        int row = 0;;
-
-        for (int column = 0; column < myArray[row].length; column++){
-            for (row = 0; row < myArray.length; row++){
-                System.out.println(myArray[row][column]);
+        for (int i = 0; i < myArray.length; i++){
+            if(myArray[i].length > maxLength){
+                maxLength = myArray[i].length;
             }
+        }
+
+        for (int column = 0; column < maxLength; column++){
+            int sum = 0;
+            for(int row = 0; row < myArray.length; row++){
+                if (column < myArray[row].length){
+                    sum += myArray[row][column];
+                }
+                else {
+                    continue;
+                }
+            }
+
+            System.out.println("Sum at column " + (column + 1) + ": "+ sum);
         }
     }
 }
