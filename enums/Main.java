e102edbe9e9d845a81ed2e09f42ba485314f6a94
simon Kachepa <kachepasimon@gmail.com>
2026-07-01 08:16:28 +0200

    Created first enum project to display trafic condition using enum

---
+++ enums/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+
+    public static void main(String[] args){
+        
+        TraficLight traficLight = TraficLight.GREEN;
+
+        System.out.println("Trafic Light condition: " + traficLight);
+    }
+}
---
+++ enums/TraficLight.java
@@ -0,0 +1,3 @@
+public enum TraficLight {
+    RED, AMBER, GREEN;
+}
