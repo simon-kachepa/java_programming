6310eee00e1eab492a9ae99778028dd28aaadaed
simon Kachepa <kachepasimon@gmail.com>
2026-07-01 08:38:05 +0200

    Give number to each enum value and adding getter for the number

--- enums/Main.java
+++ enums/Main.java
@@ -2,12 +2,20 @@ public class Main{
 
     public static void main(String[] args){
         
-        TraficLight traficLight = TraficLight.GREEN;
+        TraficLight traficLight = TraficLight.AMBER;
 
+        // Printing the enum value
         System.out.println("Trafic Light condition: " + traficLight);
 
+        // Printing the enum value's number, ie RED = 1;
+        System.out.println(traficLight.getLightValue());
+        
+        /* 
+        // Looping through the enum values...
         for (TraficLight condition : TraficLight.values()){
             System.out.println(condition);
         }
+        */
+
     }
 }
--- enums/TraficLight.java
+++ enums/TraficLight.java
@@ -1,3 +1,19 @@
 public enum TraficLight {
-    RED, AMBER, GREEN;
+    RED(1), AMBER(2), GREEN(3);
+
+    private final int lightValue;
+
+    TraficLight(int lightValue){
+        this.lightValue = lightValue;
+    }
+
+    public void displayConditions(){
+        for (TraficLight condition: values()){
+            System.out.println(condition);
+        }
+    }
+
+    public int getLightValue(){
+        return this.lightValue;
+    }
 }
