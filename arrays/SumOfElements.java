18bd6727fa221560316d8140ee7eeed6ca68c4af
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2026-08-29 23:36:10 +0200

    Update SumOfElements.java

--- arrays/SumOfElements.java
+++ arrays/SumOfElements.java
@@ -1,9 +1,17 @@
 // Program that prints the sum, product and average of the elements of an array.
 
+import java.util.Scanner;
+
 public class SumOfElements {
     
     public static void main(String[] args){
-        int[] myArray = {10, -2, 8, 3, 6, 5};
+        int[] myArray = new Array[5];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter elements of the array");
+        for(int i = 0; i < myArray.length; i++){
+            System.out.println("Enter element " + i + ": ");
+            myArray[i] = scanner.nextInt();
+        }
         int sum = 0;
         int product = 1;
         double average;
