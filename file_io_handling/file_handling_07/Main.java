f5a85dd05dbd2fba14c5873093964aa870cb5566
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2026-09-11 23:26:57 +0200

    Update Main.java

--- file_io_handling/file_handling_07/Main.java
+++ file_io_handling/file_handling_07/Main.java
@@ -12,7 +12,7 @@ public class Main {
     
     public static void main(String[] args){
 
-        String filePath = "/Users/rutendogono/Desktop/test.txt";
+        String filePath = "/Users/kachepasimon/Desktop/test.txt";
         try(FileInputStream inputStream = new FileInputStream(filePath)){
             
             int i;
